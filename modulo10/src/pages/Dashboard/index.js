524a8a77b43b399c7764e9bc702b8ceaa04b3b30
GislaineJessica <GislainehJessica@gmail.com>
2019-11-13 13:45:21 -0300

    Lista de Agendamentos

--- modulo10/src/pages/Dashboard/index.js
+++ modulo10/src/pages/Dashboard/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import { View } from 'react-native'
 import Background from '~/components/Background'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
+import { Container, Title, List } from './styles'
+import Agendamento from '~/components/Agendamento'
+
+const data = [1, 2, 3, 4, 5]
+
 export default function Dashboard() {
-  return <Background />
+  return (
+    <Background>
+      <Container>
+        <Title> Agendamentos </Title>
+
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({ item }) => <Agendamento data={item} />}
+        />
+      </Container>
+    </Background>
+  )
 }
 
 Dashboard.navigationOptions = {
