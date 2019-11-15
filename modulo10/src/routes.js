33b0983cac0a67fd8aa00f4a0652b29a550cdac6
GislaineJessica <GislainehJessica@gmail.com>
2019-11-15 17:08:46 -0300

    Rotas de Agendamento

--- modulo10/src/routes.js
+++ modulo10/src/routes.js
@@ -1,11 +1,18 @@
+import React from 'react'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createStackNavigator } from 'react-navigation-stack'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import SingIn from './pages/SignIn'
 import SingUp from './pages/SignUp'
 
 import Dashboard from './pages/Dashboard'
 import Profile from './pages/Profile'
+
+import Confirm from './pages/New/Confirm'
+import SelectDateTime from './pages/New/SelectDateTime'
+import SelectProvider from './pages/New/SelectProvider'
 
 export default (signedIn = false) =>
   createAppContainer(
@@ -18,6 +25,36 @@ export default (signedIn = false) =>
         App: createBottomTabNavigator(
           {
             Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm
+                },
+                {
+                  headerLayoutPreset: 'center',
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#fff',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20
+                    }
+                  }
+                }
+              ),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    size={20}
+                    name="add-circle-outline"
+                    color="rgba(255,255,255,0.6)"
+                  />
+                )
+              }
+            },
             Profile
           },
           {
