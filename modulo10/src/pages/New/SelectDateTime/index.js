777fc41be2e0a1f205226472bbfa202278ff9ae9
GislaineJessica <GislainehJessica@gmail.com>
2019-11-17 20:09:31 -0300

    config

--- modulo10/src/pages/New/SelectDateTime/index.js
+++ modulo10/src/pages/New/SelectDateTime/index.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import { View } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import Background from '~/components/Background'
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+// import { Container } from './styles'
 
 export default function SelectDateTime() {
   return <Background />
 }
 
 SelectDateTime.navigationOptions = ({ navigation }) => ({
-  title: 'Selecione o Prestador'
+  title: 'Selecione o Horário',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.goBack()
+      }}
+    >
+      <Icon name="chevron-left" size={20} color="#fff" />
+    </TouchableOpacity>
+  )
 })
